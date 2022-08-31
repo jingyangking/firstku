bfdd0468b16eec663197b62ba26588e927668a2f
jingyangking <w843641187@163.com>
2022-08-31 09:06:20 +0800

    ok

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 字符串转换
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "123---89---7--5685--9";
+           string[] s1= s.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+           string s2= string.Join("-", s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
